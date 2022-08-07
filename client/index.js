490ca9e0b473cfabd314837c8e84f177bd760965
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2022-08-07 18:06:32 +0100

    add pre-commit hook

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import HelloWorld from "./App.js";
 
+// eslint-disable-next-line no-undef
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).hydrate(HelloWorld);
